62c3cafb7f8fd88a031ca3adc0692cfc4d7d80ac
Pratyush Mishra <pratyushmishra@berkeley.edu>
2020-08-10 22:53:09 -0700

    Refactor concrete curves

--- r1cs-std/src/instantiated/ed_on_bn254/fields.rs
+++
@@ -1,8 +0,0 @@
-use crate::fields::fp::FpGadget;
-
-pub type FqGadget = FpGadget<algebra::ed_on_bn254::Fq>;
-
-#[test]
-fn test() {
-    crate::fields::tests::field_test::<_, algebra::ed_on_bn254::Fq, FqGadget>();
-}
